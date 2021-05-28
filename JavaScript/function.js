7fe7ef717008566da8cb9e54488c843885b9033b
VICTOR DANDOLINI <victordandolini@hotmail.com>
2021-05-28 01:58:11 -0300

    exercicios

---
+++ JavaScript/function.js
@@ -0,0 +1,11 @@
+// Criar app de frases
+function createPhrases() {
+    console.log("oi");
+    console.log("tudo");
+    console.log("bem?");
+    
+}
+
+// Executar a função
+createPhrases()
+console.log("Fim do Program");
---
+++ JavaScript/function2.js
@@ -0,0 +1,12 @@
+// Função anonima ou Function Expression
+const sum = function (number1,number2) {
+    total = number1 + number2;
+    return total
+}
+//sum(2, 3) //Argumentos da função
+
+let number1 = 2
+let number2 = 3
+console.log(number1);
+console.log(number2);
+console.log(`A soma dessa função é ${sum(number1, number2)}`)
